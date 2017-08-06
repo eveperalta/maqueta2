a0d93adf07c2952b79c3ec393d2b55bd3d6a927e
Cristian Caucott <criss.acv@gmail.com>
2017-08-06 14:33:08 -0400

    ver estado envio de email en logs

--- app/mailers/pdf_mailer.rb
+++ app/mailers/pdf_mailer.rb
@@ -3,5 +3,7 @@ class PdfMailer < ApplicationMailer
 	def pdf_email(email, pdf_path)
 		attachments['experiencia.pdf'] = File.read(pdf_path)
   	mail_gun_stat = mail(from: ENV['username'], to: email, subject: 'Prueba de email app maqueta')
+  	puts "email stat"
+  	puts mail_gun_stat
 	end
 end
