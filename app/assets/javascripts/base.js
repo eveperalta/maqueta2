1d8ccde42e968fbc3afeef3e699fbfaa040be3aa
Cristian Caucott <criss.acv@gmail.com>
2017-08-23 16:01:02 -0300

    Quitar prueba de tiempo en consola

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -392,7 +392,7 @@ function updateCarousel(type, delete_all, arrow_dir)
       limit = meta_temp.last_item_index + carrusel_meta.show_each;
     }
     
-    var date1 = new Date();
+    // var date1 = new Date();
     var ar_tmp = [];
     // Optimizar esto...
     // Se itera el array de productos para agregarlos al carrusel hasta el limit.
@@ -402,8 +402,8 @@ function updateCarousel(type, delete_all, arrow_dir)
     }
     meta_temp.carousel_node.slick('slickAdd', ar_tmp);
 
-    var date2 = new Date();
-    console.log("Se agrego en " + (date2 - date1) + " ms");
+    // var date2 = new Date();
+    // console.log("Se agrego en " + (date2 - date1) + " ms");
 
     // Hay que volver a obtener la referencia del boton de sgte | prev,
     // debido que al momento de agregar o borrar elementos del carrusel, este se 
